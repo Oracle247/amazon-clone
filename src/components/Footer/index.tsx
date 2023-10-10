dcab4c13bd340eaca64948b6df053dd1c39aa034
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-10-10 16:27:27 +0100

    upd

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
         <div>
             <footer className="flex flex-col justify-center items-center">
                 <div className="flex w-full bg-[#37475a] py-4 text-xs font-medium">Back To Top</div>
-                <div className="flex w-full">
+                <div className="flex w-full bg-[#232f3e]">
                     <div className="flex flex-col">
                         <h2 className='text-sm'>Get To Know Us</h2>
                         <p className='text-sm'>Carrer</p>
