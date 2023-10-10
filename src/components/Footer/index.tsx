c3069385ce30c0e4d2e50fffadb8c717882ec36b
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-10-10 16:26:07 +0100

    style update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,13 @@ const index = () => {
                 <div className="flex w-full bg-[#37475a] py-4 text-xs font-medium">Back To Top</div>
                 <div className="flex w-full">
                     <div className="flex flex-col">
-                        <h2>Get To Know Us</h2>
+                        <h2 className='text-sm'>Get To Know Us</h2>
+                        <p className='text-sm'>Carrer</p>
+                        <p className='text-sm'>Blog</p>
+                        <p className='text-sm'>About Amazon</p>
+                        <p className='text-sm'>Investor Relations</p>
+                        <p className='text-sm'>Amazon Devices</p>
+                        <p className='text-sm'>Amazon Science</p>
                     </div>
                 </div>
             </footer>
